48c28091048ddf2b6bf433d6adefe85632b046ef
박주찬 <p990805@naver.com>
2025-02-08 10:00:00 +0900

    브루트포스 1문제

---
+++ demo/alogirithm/src/BOJ_1436.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.Integer.*;
+
+public class Main {
+    static int N;
+    static boolean flag = false;
+    static int cnt = 0;
+    static int num=665;
+    static void solution(int n){
+
+        String a = String.valueOf(n);
+        char[]b = a.toCharArray();
+        for(int i =1; i<b.length-1; i++){
+            if(b[i]=='6'){
+                if(b[i-1]=='6' && b[i+1]=='6'){
+                    cnt++;
+                    if(cnt==N){
+                        flag = true;
+                        return;
+                    }
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = parseInt(br.readLine());
+
+        while(!flag) {
+            num++;
+            solution(num);
+
+        }
+
+        System.out.println(num);
+    }
+}
